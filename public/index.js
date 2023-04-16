d19c19884a9c6796f16510a1507e226eec0bed84
BarryYangi <2059484047@qq.com>
2023-04-16 18:49:16 +0800

    first commit

---
+++ public/index.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+  $.getJSON("data.json", function (data) {
+    $.each(data, function (key, value) {
+      $("#container").append(
+        '<img src="' + value + '" class="lazyload blur" >'
+      );
+    });
+  });
+  // 遍历所有带有“lazyload”类的图片
+  $(".lazyload").each(function () {
+    var img = $(this);
+    // 将图片的真实src添加到data-lazyload属性中
+    img.attr("data-lazyload", img.attr("src"));
+    img.removeAttr("src");
+  });
+
+  // 滚动事件处理
+  $(window).scroll(function () {
+    // 遍历所有带有“lazyload”类且处于可见范围内的图片
+    $(".lazyload:visible").each(function () {
+      var img = $(this);
+      if (img.offset().top < $(window).scrollTop() + $(window).height()) {
+        // 加载图片
+        img.attr("src", img.attr("data-lazyload"));
+        img.removeClass("lazyload");
+      }
+    });
+  });
+});
